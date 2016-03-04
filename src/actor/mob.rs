3aed963e79b080400aa4a4412072716b4640e4a4
Vaelden <maugan.villatel@gmail.com>
2016-03-04 00:00:46 +0000

    Introduced a more reasonable Context, and programmed GetClosestTarget

--- src/actor/mob.rs
+++ src/actor/mob.rs
@@ -7,14 +7,14 @@ use actor::ActorId;
 use entity::{Entity,EntityStore};
 use messages::{self,Command,Notification,EntityOrder};
 use scripts::{BehaviourTree};
-use ai::Context;
+use ai::{BehaviourTreeData,Context};
 
 pub struct AiActor {
     id: ActorId,
-    entities: HashSet<Id<Entity>>,
+    entity: Option<Id<Entity>>,
+    entities: HashSet<Id<Entity>>, // XXX: Do we really need this?
     tree: BehaviourTree,
-    // Behaviour Tree
-    // Behaviour Tree data
+    tree_data: BehaviourTreeData,
 }
 
 impl Debug for AiActor {
@@ -39,20 +39,28 @@ impl AiActor {
                       notifications: &mut Vec<Notification>,
                       _previous: &[Notification]) {
         // Context should give access to storage / current game state
-        let string = String::from("Hello World");
-        let mut context = Context { s: &string };
+        let me = match self.entity {
+            None => {
+                warn!("Trying to execute behaviour tree on AI without main entity {}", self.id);
+                return;
+            }
+            Some(me) => me,
+        };
+        let mut context = Context::new(me, entities, &mut self.tree_data);
         self.tree.visit(&mut context);
-        // Do something with behaviour trees
     }
     pub fn register_entity(&mut self, entity: Id<Entity>) {
+        self.entity = Some(entity);
         self.entities.insert(entity);
     }
 
     pub fn fake(tree: BehaviourTree) -> AiActor {
         AiActor {
             id: Id::new(),
+            entity: None,
             entities: Default::default(),
             tree: tree,
+            tree_data: BehaviourTreeData::new(),
         }
     }
 }
--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,11 +1,16 @@
 use std::collections::HashMap;
 
+use nalgebra::{Vec2,FloatPnt};
+
 use behaviour_tree::tree::{BehaviourTreeNode};
 use behaviour_tree::tree::{LeafNodeFactory,VisitResult};
 use behaviour_tree::parser::Value;
 use behaviour_tree::FactoryProducer;
 
-pub type ActionNode = Box<for<'a> BehaviourTreeNode<Context<'a>> + Send>;
+use id::Id;
+use entity::{Entity,EntityStore};
+
+pub type ActionNode = Box<for<'a, 'b> BehaviourTreeNode<Context<'a, 'b>> + Send>;
 //pub type ActionNodeFactory = Box<LeafNodeFactory<Output=Box<for<'a> BehaviourTreeNode<Context<'a>>>>>;
 pub type ActionNodeFactory = Box<BoxedClone<Output=ActionNode>>;
 pub type ActionNodeFactoryFactory = fn(&Option<Value>) -> Result<ActionNodeFactory,String>;
@@ -29,9 +34,55 @@ where T: Clone,
     }
 }
 
-pub struct Context<'a> {
-    pub s: &'a str,
+pub struct Context<'a, 'b> {
+    pub me: Id<Entity>,
+    pub entities: &'a mut EntityStore,
+    pub storage: &'b mut BehaviourTreeData,
 }
+
+impl <'a, 'b> Context<'a, 'b> {
+    pub fn new(
+        me: Id<Entity>,
+        entities: &'a mut EntityStore,
+        storage: &'b mut BehaviourTreeData,
+        ) -> Context<'a, 'b> {
+        Context {
+            me: me,
+            entities: entities,
+            storage: storage,
+        }
+    }
+}
+
+#[derive(Debug,Clone)]
+pub struct BehaviourTreeData {
+    map: HashMap<String,StoreKind>,
+    target: Option<Id<Entity>>,
+    path: Option<Path>,
+}
+
+impl BehaviourTreeData {
+    pub fn new() -> BehaviourTreeData {
+        BehaviourTreeData {
+            map: HashMap::new(),
+            target: None,
+            path: None,
+        }
+    }
+
+    fn set_target(&mut self, target: Option<Id<Entity>>) {
+        self.target = target;
+    }
+}
+
+#[derive(Clone,Debug)]
+pub enum StoreKind {
+    // TODO
+}
+
+// TODO
+#[derive(Clone,Debug)]
+pub struct Path;
 
 #[derive(Clone)]
 pub struct Prototype<T> {
@@ -50,7 +101,7 @@ impl <T> LeafNodeFactory for Prototype<T>
 where T: Clone, 
       T: 'static,
       T: Send,
-      T: for <'a> BehaviourTreeNode<Context<'a>> {
+      T: for <'a,'b> BehaviourTreeNode<Context<'a,'b>> {
     type Output = ActionNode;
     fn instanciate(&self) -> Self::Output {
         Box::new(self.inner.clone())
@@ -63,7 +114,7 @@ pub struct PrintText {
     pub text: String,
 }
 
-impl <'a> BehaviourTreeNode<Context<'a>> for PrintText {
+impl <'a,'b> BehaviourTreeNode<Context<'a,'b>> for PrintText {
     fn visit(&mut self, _context: &mut Context) -> VisitResult {
         println!("Message node: {}", self.text);
         VisitResult::Success
@@ -79,6 +130,41 @@ pub fn print_text(options: &Option<Value>) -> Result<ActionNodeFactory, String> 
     let message = message_orig.replace("_"," ");
 
     Ok(Box::new(Prototype::new(PrintText { text: message })))
+}
+
+#[derive(Clone)]
+pub struct GetClosestTarget {
+    max_sqdistance: f32,
+}
+
+impl <'a,'b> BehaviourTreeNode<Context<'a,'b>> for GetClosestTarget {
+    fn visit(&mut self, context: &mut Context) -> VisitResult {
+        let (me, mut others) = match context.entities.get_mut_wrapper(context.me) {
+            None => {
+                warn!("Main entity {} was not found in entities list", context.me);
+                return VisitResult::Failure;
+            }
+            Some((me, others)) => (me, others),
+        };
+        let my_position = me.get_position();
+        let mut closest_other = None;
+        let mut closest_other_sqdistance = self.max_sqdistance;
+        for other in others.iter() {
+            let sqdistance = my_position.sqdist(&other.get_position());
+            if sqdistance < closest_other_sqdistance {
+                closest_other = Some(other.get_id());
+                closest_other_sqdistance = sqdistance;
+            }
+        }
+        context.storage.target = closest_other;
+        println!("Get closest target: found {:?} at sqdist {}", closest_other, closest_other_sqdistance);
+        VisitResult::Success
+    }
+}
+
+pub fn get_closest_target(options: &Option<Value>) -> Result<ActionNodeFactory, String> {
+    // TODO
+    Ok(Box::new(Prototype::new(GetClosestTarget { max_sqdistance: 10000.0 })))
 }
 
 #[derive(Default)]
@@ -122,6 +208,7 @@ impl LeavesCollection {
     pub fn standard() -> LeavesCollection {
         let collection = insert_all!(
             "print_text" => print_text,
+            "get_closest_target" => get_closest_target,
             //"increment" => increment,
 
             );
